e381c3b4bad78248afcc4650f7a118e15e344195
86 <86>
2017-06-22 20:28:14 -0300

    +clamped camera position +camera shake

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -5,21 +5,30 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour {
 
 	Transform cam;
+	Animator camAnim;
 	Transform player;
 	Vector3 pos;
+	public Vector2 mapSize;
 
 	// Use this for initialization
 	void Start () {
-		cam = Camera.main.transform;
+		cam = Camera.main.transform.parent;
+		camAnim = cam.GetComponent<Animator> ();
 		pos = cam.position;
 
 		player = PlayerManager.instance.GetPlayer ().transform;
+
+		PlayerManager.instance.GetPlayer ().hpUpdateEvent.AddListener (OnPlayerHit);
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		pos.x = player.position.x;
-		pos.y = player.position.y;
+		pos.x = Mathf.Clamp(player.position.x,-mapSize.x,mapSize.x);
+		pos.y = Mathf.Clamp(player.position.y,-mapSize.y,mapSize.y);
 		cam.transform.position = pos;
+	}
+
+	void OnPlayerHit(){
+		camAnim.Play ("shake" + Random.Range(1,3));
 	}
 }
